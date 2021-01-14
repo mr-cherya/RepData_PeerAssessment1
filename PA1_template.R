8e84de54e61005f3ca1b5be7e12f7427ada7af4e
mr-cherya <mrcherya@gmail.com>
2021-01-14 07:57:42 +0200

    Project done

--- PA1_template.R
+++ PA1_template.R
@@ -1,42 +1,27 @@
----
-title: "Reproducible Research: Assessment 1"
-output: html_document
----
-
-
-1. Load and set data
-
-```{r}
+##1.load and set data
+getwd()
 setwd("C:/Users/chern/Documents/R/RepData_PeerAssessment1")
-directory <- "RepData_PeerAssessment1"
+directory <- "Part2Week3"
 unzip(zipfile="activity.zip")
 
 data <- read.csv("activity.csv", header = TRUE)
 
 SpD <- tapply(data$steps, data$date, sum)
-```
 
-
-2. Histogram of the total number of steps taken each day
-```{r}
+## 2. Histogram of the total number of steps taken each day
+png("1.png", width=600, height=700)
 hist(SpD, xlab = "Number of Steps", main = "Steps per Day")
+dev.off()
 
-```
-
-
-3. Mean and median number of steps taken each day
-
-```{r}
+## 3. Mean and median number of steps taken each day
 mean(SpD, na.rm=TRUE)
 median(SpD, na.rm=TRUE)
-```
 
-4. Time series plot of the average number of steps taken
-
-```{r}
+## 4. Time series plot of the average number of steps taken
 library(ggplot2)
 av <- aggregate(x=list(steps=data$steps), by=list(interval=data$interval),
                                 FUN=mean, na.rm=TRUE)
+png("2.png", width=800, height=700)
 
 ggplot(data=av, aes(x=interval, y=steps)) +
      geom_line() +
@@ -45,20 +30,13 @@ ggplot(data=av, aes(x=interval, y=steps)) +
      ggtitle("
              Average number of steps
              ")
-```
 
+dev.off()
 
-
-5. The 5-minute interval that, on average, contains the maximum number of steps
-
-```{r}
+## 5. The 5-minute interval that, on average, contains the maximum number of steps
 av[which.max(av$steps),]
-```
 
-
-6. Code to describe and show a strategy for imputing missing data
-
-```{r}
+## 6. Code to describe and show a strategy for imputing missing data
 missing <- sum(is.na(data$steps))
 print(missing)
 
@@ -72,23 +50,19 @@ missing.value <- function(steps, interval) {
 }
 new.data <- data
 new.data$steps <- mapply(missing.value, new.data$steps, new.data$interval)
-```
 
 
-7. Histogram of the total number of steps taken each day after missing values are imputed
-
-```{r}
+## 7. Histogram of the total number of steps taken each day after missing values are imputed
 new.steps <- tapply(new.data$steps, new.data$date, FUN=sum)
-
+png("3.png", width=600, height=700)
 hist(new.steps, xlab = "Number of Steps", main = "Steps per Day without NA")
+dev.off()
 
 mean(new.steps, na.rm=TRUE)
 median(new.steps, na.rm=TRUE)
-```
 
-8. Panel plot comparing the average number of steps taken per 5-minute interval across weekdays and weekends
+## 8. Panel plot comparing the average number of steps taken per 5-minute interval across weekdays and weekends
 
-```{r}
 days <- function(date) {
      weekdays <- days(date)
      if (weekdays %in% c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"))
@@ -99,22 +73,16 @@ days <- function(date) {
           stop("invalid date")
 }
 new.data$date <- as.Date(new.data$date)
-new.data$weekdays <- sapply(new.data$date, FUN=weekdays)
-```
+new.data$day <- sapply(new.data$date, FUN=weekend)
 
+av <- aggregate(steps ~ interval + weekdays, data = new.data, mean)
 
-```{r}
-
-
-av <- aggregate(steps ~ interval + weekdays, data=new.data, mean)
-
-ggplot(av, aes(interval, steps)) + geom_line() + facet_grid(weekdays ~ .) + 
+png("4.png", width=600, height=700)
+ggplot(av, aes(interval, steps)) + geom_line() + facet_grid(day ~ .) + 
      ggtitle("
-             Average number across weekdays and weekends
-             ")+
-     xlab("")+
+             Average number of steps taken per 5-minute interval across weekdays and weekends
+             ")
+     xlab("5-minute interval")+
      ylab("Number of steps")
-```
 
-
-
+dev.off()
